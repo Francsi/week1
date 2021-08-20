cab0faa84c2efda1a32871a5c4ef74a80fb684c1
Francis Avinash <88837938+Francsi@users.noreply.github.com>
2021-08-20 17:54:21 +0530

    Add files via upload

---
+++ inheritance/Animal.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public abstract class Animal {
+	void tailWagging() {};
+	public void sleep(String name) {
+		System.out.println(name+"sleeps while making sounds...Zzz Zzz");
+	}
+}
---
+++ inheritance/Dog.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class Dog extends Animal{
+	public void tailwagging() {
+		System.out.println("Dog wagging the tail");
+	}
+}
