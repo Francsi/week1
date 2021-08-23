e0583d256c443a81f057562a45d6787901138b3f
Francis Avinash <88837938+Francsi@users.noreply.github.com>
2021-08-23 14:30:56 +0530

    Create ListOfStudents.java

---
+++ studentList/ListOfStudents.java
@@ -0,0 +1,19 @@
+package studentList;
+import java.util.Scanner;
+
+public class ListOfStudents {
+	public static void main(String args[]) {
+		Scanner name = new Scanner(System.in);
+		System.out.println("Enter no. of students");
+		int noOfStudents = name.nextInt();
+		String[] studentList = new String[noOfStudents];
+		for(int i=0;i<noOfStudents;i++) {
+			System.out.println("Enter student name "+(i+1)+" ");
+			studentList[i] = name.nextLine();
+		}
+		for(int j=0;j<noOfStudents;j++) {
+			System.out.println((j+1)+" Name of the student "+studentList[j]);
+		}
+		name.close();
+	}
+}
