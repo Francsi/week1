aa4471b680f0faa3d591c4b907b80f8fa19af1f2
Francis Avinash <88837938+Francsi@users.noreply.github.com>
2021-09-09 12:36:40 +0530

    Update DisplayDetails.java

--- map/DisplayDetails.java
+++ map/DisplayDetails.java
@@ -1,14 +1,15 @@
 package maps;
 import java.util.*;
-public class DisplayDetails extends Details{
+public class DisplayDetails {
 	public void display() {
-		int length = records.toArray().length;
+		System.out.println(Details.records.toString());
+		int length = Details.records.toArray().length;
 		if(length != 0) {
 			for(int i=0;i<length;i++) {
-				Map<String,String> students = records.get(i); 
-				System.out.println("Name of the student is: "+students.get("name"));
-				System.out.println("EmailId of the student is: "+students.get("EmailId"));
-				System.out.println("Phone number of the student is: "+students.get("phone number")+"\n");
+				Map<String,String> students = Details.records.get(i); 
+				System.out.println("Name of the student is: "+students.get("name")+
+						"\nEmailId of the student is: "+students.get("emailId")+
+						"\nPhone number of the student is: "+students.get("phone number")+"\n");
 			}
 		}
 		else {
