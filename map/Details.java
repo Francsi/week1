f4d504122b8272737952b48c6ac6d0990f086742
Francis Avinash <88837938+Francsi@users.noreply.github.com>
2021-09-09 12:36:10 +0530

    Update Details.java

--- map/Details.java
+++ map/Details.java
@@ -2,5 +2,5 @@ package maps;
 import java.util.*;
 
 public class Details {
-	List<Map<String,String>> records = new ArrayList<Map<String,String>>();
+	static List<Map<String,String>> records = new ArrayList<Map<String,String>>();
 }
